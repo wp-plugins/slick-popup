75e9d0ef98c0a5a0f7b7d35b69ab0e9b1d690268
ak.singla <ak.singla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-23 17:46:05 +0000

    Updated plugin head and readme.txt stable tag for releasing the update 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/slick-popup/trunk@1097404 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- slick-popup.php
+++ slick-popup.php
@@ -7,7 +7,7 @@ Plugin URI:   http://www.omaksolutions.com
 Description:  Beautiful, Slick and customizable pop-up compatible with Contact Form 7.
 Author URI:   http://www.omaksolutions.com 
 Author:       Om Ak Solutions 
- Version:      1.1.1
+ Version:      1.2.0
 */
 
 //include 'other-options.php';
