7eafa4a3d1f54b39c7ffa54c78d89246523622e8
ak.singla <ak.singla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-01 22:37:25 +0000

    * Better Color picker in plugin options. * Displaying version number in plugin head. * Files edited: slick-options.php, slick-popup.php, admin-script.js * Updating to 1.2.2
    git-svn-id: https://plugins.svn.wordpress.org/slick-popup/tags/1.2.2@1102822 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ custom.js
@@ -0,0 +1,75 @@
+function unloadPopupBox() {    // TO Unload the Popupbox
+
+	var docHeight = jQuery(document).height();
+
+	jQuery('#popup_box').fadeOut("slow");
+	jQuery(".container").css({ // this is just for style       
+		"opacity": "1" 
+	});
+	jQuery('#curtain').fadeOut("slow");
+	return false;	
+}   
+			   
+function loadPopupBox() {  
+
+	var docHeight = jQuery(document).height();
+	
+	jQuery("html, body").animate({scrollTop: 0}, 1000);
+	jQuery('#curtain').height(docHeight);
+	jQuery('#curtain').fadeIn("slow");		
+	
+	jQuery('#popup_box').fadeIn("slow");
+	jQuery(".container").css({ // this is just for style
+		"opacity": "0.3" 
+	});
+	
+	set_popup();
+	
+	return false;
+} 
+jQuery( window ).resize(function() {
+	set_popup();
+});
+
+
+jQuery(document).ready(function($) {
+	set_popup();
+});
+
+function set_popup() {
+	popup = jQuery('#popup_box');
+	curtain = jQuery('#curtain');
+	
+	if ( popup.length ) {
+		popupHeight = popup.outerHeight();
+		titleHeight = jQuery('#popup_title').outerHeight();
+		descriptionHeight = jQuery('#popup_description').outerHeight();
+		
+		descrptionHeight = 0; 
+		//alert( titleHeight ); 
+		
+		formHeight = popupHeight - titleHeight - descriptionHeight - 0;
+		//alert( 'Popup:' +popupHeight+ ' + FormHeight:' +formHeight );
+		$formContainer = jQuery('#form_container').height(formHeight);
+		
+		
+		// Set Popup Left Position
+		curtainWidth = curtain.width();
+		popupWidth = popup.width();
+		
+		popLeft = (curtainWidth - popupWidth)/2;
+		//alert(popLeft);
+		
+		popup.css( 'left', popLeft+'px' );
+	}
+	set_right_button();
+}
+
+function set_right_button() {
+	side_button = jQuery('.side-enquiry');
+	curtain = jQuery('#curtain');
+	
+	button_width = side_button.width(); 
+	
+	side_button.css( 'right', '-'+button_width/2+'px' );
+}
