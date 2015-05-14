ea270958e8489387045739f7b5ab8be2039eabad
ak.singla <ak.singla@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-14 13:30:42 +0000

    * Fixed - Popup activator selector jQuery code error.
    git-svn-id: https://plugins.svn.wordpress.org/slick-popup/tags/1.2.6@1160450 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,93 @@
+=== Slick Popup ===
+Contributors: ak.singla
+Donate link: http://www.slick-popup.omaksolutions.com/donate
+Tags: popup, cf7 compatible
+Requires at least: 3.0.1
+Tested up to: 4.2.1
+Stable tag: 1.2.6
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+A CF7 compatible pop-up form which is slick, beautiful and responsive to different screen-sizes.
+
+== Description ==
+
+A Contact Form7 compatible pop-up form. The pop-up is responsive to different screen-sizes. Gives you a pop-up form that aids your visitors
+to contact you easily. Enjoy with the plugin. 
+
+The plugin converts a Contact Form 7 form in to a beautiful popup. So, Contact Form 7 plugin must be installed and setup to use Slick Popup. 
+
+*   Responsive and browser compatible
+*   Choose color scheme as per your theme
+*   "Get a quote" or "Have a query" buttons
+
+[Plugin Demo](http://www.slick-popup.omaksolutions.com/)
+
+After the new color picker in the plugin options, choosing your color scheme is easier. You will enjoy using it. 
+
+== Installation ==
+
+The quickest method for installing the Slick Popup plugin is:
+
+1. Visit Plugins -> Add New in the WordPress dashboard
+2. Search for "Slick Popup"
+3. Click "Install Now"
+4. Finally click "Activate Plugin"
+
+If you would prefer to do things manually then follow these instructions:
+
+1. Upload the `slick-popup` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Edit settings from the Dashboard Options page "Slick Popup"
+
+== Frequently Asked Questions ==
+
+= I have a doubt, what should I do? =
+
+F.A.Q section will be completed soon. Till then you can use the plugin's support forum or mail me at: ak.singla@hotmail.com
+
+== Screenshots ==
+
+1. Plugin Options in the WP backend. 
+2. Enquiry form pops up on clicking the button on the right. 
+3. Popup form layout available on all pages. 
+4. Side button position on smaller screens. 
+
+== Changelog ==
+
+= 1.2.6 =
+* Fixed: External Selector for activating the popup had a type error.
+
+= 1.2.5 =
+* Added: External Selector for activating the popup. Default Selector: .popup-activator
+
+= 1.2.4 =
+* Added: Notice about Slick Popup plugin in plugin options
+
+= 1.2.3 =
+* Fixed side button z-index placing
+
+= 1.2.2 =
+* Better Color picker in plugin options. 
+* Version number in plugin head on options page.
+
+= 1.2.1 =
+* Side button option: "Contact Us" text added
+
+= 1.2.0 =
+
+= 1.2.0 =
+* Side Button Improved for using Text and not images
+* Side Button position made to left on smaller screens
+
+= 1.0 =
+* Improved Options Page
+* Few Others Tests and Improvisations
+
+= 0.1 =
+* First Uploaded to WordPress
+
+== Upgrade Notice ==
+
+= 0.1 =
+* First Uploaded to WordPress
---
+++ slick-popup.php
@@ -0,0 +1,294 @@
+<?php
+ /*
+=== [Slick Popup] ===
+
+Plugin Name:  Slick Popup
+Plugin URI:   http://www.omaksolutions.com
+Description:  Beautiful, Slick and customizable pop-up compatible with Contact Form 7.
+Author URI:   http://www.omaksolutions.com 
+Author:       Om Ak Solutions 
+Version:      1.2.6
+*/
+
+//include 'other-options.php';
+
+add_action( 'admin_bar_menu', 'toolbar_link_to_slick_popup', 999 );
+function toolbar_link_to_slick_popup( $wp_admin_bar ) {
+	if ( current_user_can('manage_options') ) {
+		$args = array(
+			'id'    => 'slick_popup',
+			'title' => 'Slick Popup',
+			'href'  => admin_url('admin.php?page=slick-options.php'),
+			'meta'  => array( 'class' => 'my-toolbar-page' )
+		);
+		$wp_admin_bar->add_node( $args );
+	}
+}
+
+//SETUP
+register_activation_hook(__FILE__,'mypopup_install'); 
+function mypopup_install(){
+	// Empty Activation Hook
+}
+
+
+/////////////////////////////////////////
+// Initialise the plugin and scripts
+/////////////////////////////////////////
+add_action('init','slick_popup_init');
+function slick_popup_init(){
+    add_html_and_scripts();
+}
+
+/////////////////////////////////////////
+// Add Html And Scripts
+/////////////////////////////////////////
+function add_html_and_scripts(){
+	// Add Pop Up to Footer Here
+	add_action('wp_footer', 'add_my_popup');
+	add_action('wp_footer', 'option_css');
+	add_action( 'wp_enqueue_scripts', 'enqueue_popup_scripts' );	
+}
+
+
+// Create Menu Page
+add_action('admin_menu', 'my_plugin_menu');
+function my_plugin_menu() {
+	
+	// Page Title, Page Menu Name, Capability, 'Admin Page Unique Slug, Fallback 
+	// http://codex.wordpress.org/Function_Reference/add_options_page
+	add_menu_page('Slick Popup Options', 'Slick Popup', 'manage_options', 'slick-options.php', 'my_plugin_options');
+	
+	//call register settings function
+	add_action( 'admin_init', 'register_settings' ); 
+}
+
+// Tell whih is our admin page
+function my_plugin_options(){
+     include('admin/slick-options.php');
+} 
+
+// Tell what are our options
+function register_settings() {
+
+	add_settings_section(
+		'layout_section',
+		'Layout Settings',
+		'layout_section_callback_function',
+		'layout'
+	);
+	
+	$fields = array(
+		array( 'id'=>'primary_color', 'title'=>'Primary Color', 'callback'=>'', 'menupage'=>'slick-options.php', 'section'=>'default' )
+	);
+		//register our settings
+	register_setting( 'mypopup-group', 'primary_color' );
+	register_setting( 'mypopup-group', 'border_color' );
+	
+	register_setting( 'mypopup-group', 'form_title' );
+	register_setting( 'mypopup-group', 'form_description' );
+	
+	register_setting( 'mypopup-group', 'cf7_shortcode' );
+	register_setting( 'mypopup-group', 'cf7_id' );
+	register_setting( 'mypopup-group', 'cf7_title' );
+	
+	register_setting( 'mypopup-group', 'side_image' );
+	
+	
+	
+	$icons = array( 'fb', 'twitter', 'gplus', 'linkedin', 'rss', 'website' );
+	
+	foreach( $icons as $icon ) {
+		register_setting( 'mypopup-group', $icon. '_url' );
+		register_setting( 'mypopup-group', $icon. '_alt' );
+	}
+	
+} 
+
+
+function add_my_popup() {
+	if( !is_admin() ) {
+		$cf7_code = get_option('cf7_shortcode')?get_option('cf7_shortcode'):'[contact-form-7 id="142" title="Contact Page Form"]';
+		$cf7_id = get_option('cf7_id')?get_option('cf7_id'):'1';
+		$cf7_title = get_option('cf7_title')?get_option('cf7_title'):'Submit Paper Form';
+		
+		$form_title = get_option('form_title')?get_option('form_title'):'Contact Us - We care to help!';
+		$form_description = get_option('form_title')?get_option('form_description'):'Please fill our short form and one of our friendly team members will call you back.';
+		$side_image = get_option('side_image')?get_option('side_image'):'have-a-query';	
+		switch( $side_image ) {
+			case 'have-a-query': $side_text = 'Have a Query?'; break;			
+			case 'get-a-quote': $side_text = 'Get a Quote'; break;	
+			default: $side_text = 'Contact Us'; 
+		}
+		?>
+		<!-- Pop Up Box and Curtain Arrangement -->
+			<div id="curtain" onClick="unloadPopupBox();" style=""></div>
+			<div id="popup_box">  
+				<div id="popup_title">
+					<?php echo $form_title; ?>
+				</div>
+				<div id="form_container">
+					<p id="popup_description">
+						<?php echo $form_description; ?>
+					</p>
+					
+					<?php
+						//echo $cf7_code; 
+						echo do_shortcode( '[contact-form-7 id="' .$cf7_id. '" title="' .$cf7_title. '"]'); 
+					?>
+				</div>
+				<!--<div class="success" style="display: none;">Successfully Submitted ...</div>-->
+			   <a id="popupBoxClose" onClick="unloadPopupBox();">X</a>  
+			</div>
+			<!--<div  class="side-enquiry-holder holder-<?php echo $side_image; ?>" >
+				<a onClick="loadPopupBox();" class="side-enquiry <?php echo $side_image; ?>">Have a query?</a>
+			</div>-->
+			<a onClick="loadPopupBox();" class="side-enquiry"><?php echo $side_text ?></a>
+			<!-- Pop Up Box and Curtain Arrangement -->
+<?php
+	}
+}
+
+function option_css() {
+	
+	$primary_color = get_option('primary_color')?get_option('primary_color'):'#074C97';
+	$border_color = get_option('border_color')?get_option('border_color'):'#276AB2';
+	
+	?>
+	
+	<?php if( !is_admin() ) { ?>
+		<style>
+			#popup_box .wpcf7-form-control.wpcf7-submit,
+			.side-enquiry-holder, #popup_title, .side-enquiry {
+				background: <?php echo $primary_color; ?>;
+			}
+			#popup_box {
+				border: 3px solid <?php echo $border_color; ?>;
+			}
+		</style>
+<?php	
+	}
+}
+
+function enqueue_popup_scripts() {
+	if ( !is_admin() ) {
+		wp_register_style( 'popup-css', plugins_url( '/css.css', __FILE__ ) );
+		wp_enqueue_style( 'popup-css' ); 
+		
+		wp_register_script( 'popup-js', plugins_url( '/custom.js', __FILE__ ) );
+		wp_enqueue_script( 'popup-js' ); 
+	}
+}
+
+
+
+function create_share_button() {
+
+	$atts = shortcode_atts( array(
+		'fb' => array(
+			'url' => 'http://www.facebook.com/OmAkSolutions',
+			'alt' => 'Like us on Facebook',
+			'class' => 'fb',
+		),
+		'twitter' => array(
+			'url' => 'https://twitter.com/SinglaAk',
+			'alt' => 'Follow us on Twitter',
+			'class' => 'twitter',
+		),
+		'gplus' => array(
+			'url' => 'http://in.linkedin.com/pub/ankit-singla/62/647/4b4',
+			'alt' => 'Connect on Gplus',
+			'class' => 'gplus',
+		),
+		'linkedin' => array(
+			'url' => 'http://in.linkedin.com/pub/ankit-singla/62/647/4b4',
+			'alt' => 'Know us on LinkedIn',
+			'class' => 'linkedin',
+		),
+		'rss' => array(
+			'url' => 'http://in.linkedin.com/pub/ankit-singla/62/647/4b4',
+			'alt' => 'Subscribe to RSS',
+			'class' => 'rss',
+		),
+		'website' => array(
+			'url' => 'http://in.linkedin.com/pub/ankit-singla/62/647/4b4',
+			'alt' => 'Web Presence',
+			'class' => 'website',
+		)
+	), $atts );
+	
+	 
+	 // Get Values From Options Page
+	$icons = array( 'fb', 'twitter', 'gplus', 'linkedin', 'rss', 'website' );
+		foreach( $icons as $icon ) {
+			$atts[$icon]['url'] = get_option($icon. '_url') ? get_option($icon. '_url') : '';
+			//$atts[$icon]['alt'] = get_option($icon. '_alt') ? get_option($icon. '_alt') : '';
+		}
+	
+	$output = ''; 
+	$is_True = True;
+	
+	//$output .= '<br/>KeyAtts: ' .key($atts);
+	if( 0 ) {
+		foreach( $atts as $attri ) {
+			if( isset($attri['url']) AND ! empty($attri['url']) ) {
+				$output .= '<br/>Key: ' .key($attri). ' URL: ' .$attri['url']. ' Alt: ' .$attri['alt'];
+			}
+		}
+	}
+	
+	$output .= '<ul class="omaksociallinks">';
+	
+	foreach( $atts as $attri ) {
+		
+		if( isset($attri['url']) AND ! empty($attri['url']) ) {
+			
+			if( $attri['class'] == 'website' )
+				$target_window = ''; 
+			else 
+				$target_window = ' target="_blank" '; 
+				
+			$output .= '<li class="' .$attri['class']. '">';
+				$output .= '<a href="' .$attri['url']. '" title="' .$attri['alt']. '"' .$target_window. '>';
+					//$output .= $attri['alt'];
+					$output .= 'A';
+				$output .=  '</a>';
+			$output .= '</li>';
+			$output .= '<!-- icon icon-' .$attri['class']. '-->';
+		}
+	}
+	
+	$output .= '</ul>';
+	/*
+	$output .= '<div class="omaksharer">';
+		$output .= '<a onclick="' .$onClick. '" href="http://www.facebook.com/sharer/sharer.php?u=' .get_the_permalink(). '&t=' .get_the_title(). '" class="fb-share" id="onclick-popup">';
+			$output .= '<img class="fb-icon" src="' .plugins_url( 'images/fb-icon.png', __FILE__ ). '" style="border:0 !important;">';
+			$output .= '<span class="share-text">' .$atts['button_text']. '</span>';
+			$output .= '<span id="fb-share-count" style="">';
+				$output .= '<img src="' .plugins_url( 'images/arrow-white.png', __FILE__ ). '" style="border:0 !important;">';
+				$output .= $fb_data;
+			$output .= '</span>';
+		$output .= '</a>';
+	$output .= '</div>';
+	*/
+	return $output;
+}
+add_shortcode( 'OmakSocialLinks', 'create_share_button' ); 
+
+
+
+////////////////////////////////////
+// Admin-Script.JS added
+// With dependency on wp-color-picker.com
+// Help: https://make.wordpress.org/core/2012/11/30/new-color-picker-in-wp-3-5/
+////////////////////////////////////
+add_action( 'admin_enqueue_scripts', 'mw_enqueue_color_picker' );
+function mw_enqueue_color_picker( $hook_suffix ) {
+    // first check that $hook_suffix is appropriate for your admin page
+	if( $hook_suffix != 'toplevel_page_slick-options' )
+		return;
+    wp_enqueue_style( 'wp-color-picker' );
+    wp_enqueue_script( 'my-script-handle', plugins_url('admin-script.js', __FILE__ ), array( 'wp-color-picker' ), false, true );
+}
+
+?>
